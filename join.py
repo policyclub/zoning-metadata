5ee697b13478b9964587155f8f7781db853c5362
hunterowens <owens.hunter@gmail.com>
2018-01-24 16:41:36 -0800

    adding base files

--- join.py
+++ join.py
@@ -11,13 +11,21 @@ merged = zoning.merge(zoning_metadata, on='zone')
 
 print("Lost {} parcels in merged from original list of {}".format(len(zoning) - len(merged),
                                                                    len(zoning)))
-qual_params = ['sb_827_parking', 'sb_827_FAR', 'sb_827_res_density', 
+query_params = ['sb_827_parking', 'sb_827_FAR', 'sb_827_res_density', 
                'sb_827_height_quarter_mile','sb_827_height_half_mile']
 
-qual_prop = merged[qual_params]
-qual_prop = qual_prop[qual_prop.sb_827_non_res == False]
+qual_prop = merged[merged[query_params].any(axis=1)]
 
+print("num of qualified zones is ", len(qual_prop))
+qual_prop = qual_prop[qual_prop['sb_827_non_res'] == False]
+
+print("num of qualified minus non res is ", len(qual_prop))
 hqtas = gpd.read_file('./transit-service/SCAG-hqta.geojson')
 aff_prop = gpd.sjoin(merged, hqtas, op='intersects', how='inner')
 
-aff_prop.to_file('./la.geojson', driver='GeoJSON')
+height_changes = aff_prop[aff_prop['sb_827_height_quarter_mile'] == True]
+
+density_or_parking_changes = aff_prop[aff_prop['sb_827_parking'] == True]
+
+height_changes.to_file('./height.geojson', driver='GeoJSON')
+density_or_parking_changes.to_file('./parking_changes.geojson', driver='GeoJSON')
